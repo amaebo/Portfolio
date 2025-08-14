896b9aeb7c8bac9eba6bf0ff6ecfb3d360803ff5
amaebo <amaebong20033@gmail.com>
2025-08-13 20:03:05 -0600

    Toggle nav menu via hamburger and close Icon

--- Project/Scripting/script.js
+++ Project/Scripting/script.js
@@ -1,6 +1,21 @@
-// const menuToggle = document.getElementById('menu-toggle');
-// const navLinks = document.querySelector('.nav-links');
+// Show mobile nav only when clicked on
+const mobileNav = document.getElementById("mobile-nav");
+const navToggle= document.getElementById('nav-toggle')
+const closeNav = document.getElementById("close-nav");
+const navitems = document.getElementsByClassName("nav-links")
 
-// menuToggle.addEventListener('click', () => {
-//   navLinks.classList.toggle('show');
-// });
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    mobileNav.style.display = 'grid';
+    navToggle.style.display = 'none';
+    closeNav.style.display = 'block';
+  });
+} else console.log("navToggle not found")
+
+if (closeNav) {
+  closeNav.addEventListener('click', () => {
+    mobileNav.style.display = 'none';
+    navToggle.style.display = 'block';
+    closeNav.style.display = 'none';
+  });
+}
