94bd1166d3da3b68fba17380427ea50729a4cbd1
amaebo <amaebong20033@gmail.com>
2025-08-14 15:57:32 -0600

    Enhance mobile nav interactivity by adding close functionality on link click

--- Project/Scripting/script.js
+++ Project/Scripting/script.js
@@ -2,16 +2,32 @@
 const mobileNav = document.getElementById("mobile-nav");
 const navToggle= document.getElementById('nav-toggle')
 const closeNav = document.getElementById("close-nav");
-const navitems = document.getElementsByClassName("nav-links")
+const navitems = document.getElementsByClassName("nav-links");
+const links = document.querySelectorAll(".nav-links li"); // returns a node list 
 
 if (navToggle) { //show open nav menu
   navToggle.addEventListener('click', () => {
-    mobileNav.classList.add('open');
+    mobileNav.classList.add('open'); // displays menu
+    navToggle.style.display = 'none';    // Hide hamburger
+    closeNav.style.display = 'block';    // Show close icon
   });
 }
 
 if (closeNav) { //close nav menu
   closeNav.addEventListener('click', () => {
-    mobileNav.classList.remove('open');
+    mobileNav.classList.remove('open'); //hide menu
+    navToggle.style.display = 'block';  // Show hamburger
+    closeNav.style.display = 'none';    // Hide close icon
   });
+}
+
+//close menu when list item is click
+for (const link of links){
+  link.addEventListener('click', () => {
+    if (mobileNav.classList.contains('open')){
+      mobileNav.classList.remove('open'); //hide menu
+      navToggle.style.display = 'block';  // Show hamburger
+      closeNav.style.display = 'none';    // Hide close icon
+    }
+  })
 }
