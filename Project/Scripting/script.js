e05d7a4063c11358a9d394d5eb972144da592308
amaebo <amaebong20033@gmail.com>
2025-08-14 16:13:32 -0600

    Refactor mobile navigation icons and styles for improved clarity and functionality

--- Project/Scripting/script.js
+++ Project/Scripting/script.js
@@ -1,23 +1,23 @@
 // Show mobile nav only when clicked on
 const mobileNav = document.getElementById("mobile-nav");
-const navToggle= document.getElementById('nav-toggle')
-const closeNav = document.getElementById("close-nav");
+const navOpenIcon= document.getElementById('nav-open-icon')
+const navCloseIcon = document.getElementById("nav-close-icon");
 const navitems = document.getElementsByClassName("nav-links");
-const links = document.querySelectorAll(".nav-links li"); // returns a node list 
+const links = document.querySelectorAll(".nav-links li"); // returns a node list of li links
 
-if (navToggle) { //show open nav menu
-  navToggle.addEventListener('click', () => {
+if (navOpenIcon) { //show open nav menu
+  navOpenIcon.addEventListener('click', () => {
     mobileNav.classList.add('open'); // displays menu
-    navToggle.style.display = 'none';    // Hide hamburger
-    closeNav.style.display = 'block';    // Show close icon
+    navOpenIcon.style.display = 'none';    // Hide hamburger
+    navCloseIcon.style.display = 'block';    // Show close icon
   });
 }
 
-if (closeNav) { //close nav menu
-  closeNav.addEventListener('click', () => {
+if (navCloseIcon) { //close nav menu
+  navCloseIcon.addEventListener('click', () => {
     mobileNav.classList.remove('open'); //hide menu
-    navToggle.style.display = 'block';  // Show hamburger
-    closeNav.style.display = 'none';    // Hide close icon
+    navOpenIcon.style.display = 'block';  // Show hamburger
+    navCloseIcon.style.display = 'none';    // Hide close icon
   });
 }
 
@@ -26,8 +26,8 @@ for (const link of links){
   link.addEventListener('click', () => {
     if (mobileNav.classList.contains('open')){
       mobileNav.classList.remove('open'); //hide menu
-      navToggle.style.display = 'block';  // Show hamburger
-      closeNav.style.display = 'none';    // Hide close icon
+      navOpenIcon.style.display = 'block';  // Show hamburger
+      navCloseIcon.style.display = 'none';    // Hide close icon
     }
   })
 }
