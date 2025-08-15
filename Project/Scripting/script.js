c59c643a3209b2cd2555954a3d5f08ffd9b09672
amaebo <amaebong20033@gmail.com>
2025-08-14 18:49:34 -0600

    Refactor mobile navigation icons to improve visibility and functionality

--- Project/Scripting/script.js
+++ Project/Scripting/script.js
@@ -3,31 +3,36 @@ const mobileNav = document.getElementById("mobile-nav");
 const navOpenIcon= document.getElementById('nav-open-icon')
 const navCloseIcon = document.getElementById("nav-close-icon");
 const navitems = document.getElementsByClassName("nav-links");
-const links = document.querySelectorAll(".nav-links li"); // returns a node list of li links
+const navlinks = document.querySelectorAll(".nav-links li"); // returns a node list of li links
 
-if (navOpenIcon) { //show open nav menu
-  navOpenIcon.addEventListener('click', () => {
-    mobileNav.classList.add('open'); // displays menu
-    navOpenIcon.style.display = 'none';    // Hide hamburger
-    navCloseIcon.style.display = 'block';    // Show close icon
-  });
+
+function toggleNavIcons(isMenuOpen) {
+  if (isMenuOpen){
+    // show close icon
+    navOpenIcon.classList.add('hidden');
+    navCloseIcon.classList.remove('hidden');
+  } else {
+    // show open icon
+    navOpenIcon.classList.remove('hidden');
+    navCloseIcon.classList.add('hidden');
+  }
 }
+//open mobile nav functionality
+navOpenIcon.addEventListener('click', () => {
+  mobileNav.classList.add('open');
+  toggleNavIcons(true);
+});
 
-if (navCloseIcon) { //close nav menu
-  navCloseIcon.addEventListener('click', () => {
-    mobileNav.classList.remove('open'); //hide menu
-    navOpenIcon.style.display = 'block';  // Show hamburger
-    navCloseIcon.style.display = 'none';    // Hide close icon
-  });
-}
+//close mobile nav functionality 
+navCloseIcon.addEventListener('click', () => {
+  mobileNav.classList.remove('open');
+  toggleNavIcons(false);
+});
 
-//close menu when list item is click
-for (const link of links){
+//moblie nav list item click functionality 
+navlinks.forEach(link => {
   link.addEventListener('click', () => {
-    if (mobileNav.classList.contains('open')){
-      mobileNav.classList.remove('open'); //hide menu
-      navOpenIcon.style.display = 'block';  // Show hamburger
-      navCloseIcon.style.display = 'none';    // Hide close icon
-    }
-  })
-}
+    mobileNav.classList.remove('open');
+    toggleNavIcons(false);
+  });
+});
